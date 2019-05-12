1dfd75292a4f521ff141858e194f0c7ff4556d47
Brandy Mello <brandyamello@gmail.com>
2019-05-12 12:09:19 -0600

    Repeat dragon

--- exercises/dragon.js
+++ exercises/dragon.js
@@ -1,20 +1,66 @@
+
 class Dragon {
   constructor(name, rider, color) {
     this.name = name;
-    this.rider = rider;
-    this.color = color;
+    this.rider = 'Eragon';
+    this.color = 'blue';
     this.hungry = true;
     this.counter = 0;
   }
-    eat() {
-      this.counter ++;
-      if (this.counter >= 3) {
-        this.hungry = false;
-      }
-    }
+
+  eat() {
+    this.counter ++;
+    if (this.counter >= 3) {
+     this.hungry = false; 
+    }    
+  } 
 }
 
 module.exports = Dragon;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Dragon {
+//   constructor(name, rider, color) {
+//     this.name = name;
+//     this.rider = rider;
+//     this.color = color;
+//     this.hungry = true;
+//     this.counter = 0;
+//   }
+//     eat() {
+//       this.counter ++;
+//       if (this.counter >= 3) {
+//         this.hungry = false;
+//       }
+//     }
+// }
+
+// module.exports = Dragon;
 
 
 

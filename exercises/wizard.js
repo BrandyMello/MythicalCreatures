7eb493f58df6460ed0bf05d545dbda29b604315e
Brandy Mello <brandyamello@gmail.com>
2019-05-13 21:05:23 -0600

    Repeat wizard

--- exercises/wizard.js
+++ exercises/wizard.js
@@ -1,31 +1,81 @@
 class Wizard {
 
-  constructor(obj) {
-    this.name = obj.name;
-    this.bearded = true;
+  constructor(object) {
+    this.name = object.name;
     this.isRested = true;
-    this.spellCounter = 0;
-
-    if (obj.bearded === false) {
+    this.counter = 0;
+    this.bearded = true;
+    if (object.bearded === false) {
       this.bearded = false;
     }
   }
-
   incantation(spell) {
     return spell.toUpperCase();
   }
-
   cast() {
-    this.spellCounter ++;
-    if (this.spellCounter >= 3){
+    this.counter++;
+    if (this.counter >= 3){
       this.isRested = false;
-      return 'I SHALL NOT CAST!'
+      return "I SHALL NOT CAST!"
+    } else {
+      return 'MAGIC BULLET';
     }
-    return 'MAGIC BULLET';
   }
 }
 
 module.exports = Wizard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Wizard {
+
+//   constructor(obj) {
+//     this.name = obj.name;
+//     this.bearded = true;
+//     this.isRested = true;
+//     this.spellCounter = 0;
+
+//     if (obj.bearded === false) {
+//       this.bearded = false;
+//     }
+//   }
+
+//   incantation(spell) {
+//     return spell.toUpperCase();
+//   }
+
+//   cast() {
+//     this.spellCounter ++;
+//     if (this.spellCounter >= 3){
+//       this.isRested = false;
+//       return 'I SHALL NOT CAST!'
+//     }
+//     return 'MAGIC BULLET';
+//   }
+// }
+
+// module.exports = Wizard;
 
 
 
